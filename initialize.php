ae802a7c17844d493f95f76f326aff8db4cc252d
dalytech <49926562+dalytech@users.noreply.github.com>
2021-08-26 17:32:59 +0100

    Initial commit

---
+++ initialize.php
@@ -0,0 +1,10 @@
+<?php
+$dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
+if(!defined('base_url')) define('base_url','http://192.168.100.144/traffic_offense/');
+if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+if(!defined('dev_data')) define('dev_data',$dev_data);
+if(!defined('DB_SERVER')) define('DB_SERVER',"remotemysql.com");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"Eq65GYGEcf");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"DS7pz9V3AO");
+if(!defined('DB_NAME')) define('DB_NAME',"Eq65GYGEcf");
+?>
